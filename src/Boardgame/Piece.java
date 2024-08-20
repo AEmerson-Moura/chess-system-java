60853c004fb73ca8f153e59d4f860aac16dffe69
AEmerson-Moura <emersonsilver@outlook.com.br>
2024-08-20 20:23:58 -0300

    Starting to implement Board and Piece

---
+++ src/Boardgame/Piece.java
@@ -0,0 +1,16 @@
+package Boardgame;
+
+public class Piece {
+	protected Position position;
+	private Board board;
+	
+	public Piece() {
+	}
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+	public Board getBoard() {
+		return board;
+	}
+}
